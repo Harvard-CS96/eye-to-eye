bc5368c1248dd0be5c5f3b22a7b4e83227eb1ba0
= <nboucher@college.harvard.edu>
2017-10-22 16:51:28 -0700

    Force installation of new package dependencies

--- scripts/restartnginx.sh
+++ scripts/restartnginx.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 service nginx restart > /var/log/restartapache.out 2>&1
+npm install
 pm2 restart server
--- scripts/startnginx.sh
+++ scripts/startnginx.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 service nginx start > /var/log/startnginx.out 2>&1
+npm install
