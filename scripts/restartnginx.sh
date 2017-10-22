d61ba802590b9a0d6b902fa5dce72c22fe7db39f
= <nboucher@college.harvard.edu>
2017-10-22 16:54:10 -0700

    Added explicit install dir path

--- scripts/restartnginx.sh
+++ scripts/restartnginx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 service nginx restart > /var/log/restartapache.out 2>&1
-npm install
+npm install /var/www/html
 pm2 restart server
--- scripts/startnginx.sh
+++ scripts/startnginx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 service nginx start > /var/log/startnginx.out 2>&1
-npm install
+npm install /var/www/html
