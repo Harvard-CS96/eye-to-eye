5dd7885c9862d5a40170b09a0a86635c816426c4
Patrick Pan <patrick.pan@patrickpan.com>
2017-11-08 14:05:57 -0500

    Integrate auth with facebook

--- client/modules/user/operations.js
+++ client/modules/user/operations.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
-import * as actions from './actions';
+// import axios from 'axios';
+// import * as actions from './actions';
 
-const login = () => dispatch => {
-    return axios('/user/login')
-        .then(response => response.data)
-        .then(data => {
-            console.log(data)
-            const { id } = data;
-            dispatch(actions.login(id))
-        })
-}
+// const login = () => dispatch => {
+//     return axios('/user/login')
+//         .then(response => response.data)
+//         .then(data => {
+//             console.log(data)
+//             const { id } = data;
+//             dispatch(actions.login(id))
+//         })
+// }
 
-const logout = actions.logout;
+// const logout = actions.logout;
 
 const submitFeedback = actions.submitFeedback;
 
