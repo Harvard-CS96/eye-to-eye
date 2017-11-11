76d5f80019c616deac8119d99ae75080e71d1faa
LisaLudique <lisacarolynlu@gmail.com>
2017-11-10 23:26:23 -0500

    Fixed badge image display

--- client/components/Badge.js
+++ client/components/Badge.js
@@ -6,7 +6,11 @@ class Badge extends React.Component {
     render() {
         var badgeId = this.props.badgeId;     
         return <div id="Badge">
+<<<<<<< 5dd7885c9862d5a40170b09a0a86635c816426c4
             <img src={require("../assets/badge-" + badgeId + "-on.png")} width="100" height="100"/>
+=======
+            <img src={require("../assets/badge-" + badgeId + "-off.png")}/>
+>>>>>>> Fixed badge image display
         </div>
     }
 }
