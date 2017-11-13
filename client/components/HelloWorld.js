0bc91d9861391b04eba8187e6d3bc5d2aa31f7d3
Sam Plank <samplank@Sams-MacBook-Pro-2.local>
2017-11-13 13:57:23 -0500

    add profile

--- client/components/HelloWorld.js
+++ client/components/HelloWorld.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Counter from '../containers/Counter';
+<<<<<<< bfd2f88c7e0ff0984d0ecb09001eca1de46c9bb4
 import Feedback from '../containers/Feedback';
+=======
+>>>>>>> add profile
 import Profile from '../containers/Profile'
 
 import Strings from '../json/strings.json';
@@ -9,16 +12,23 @@ import Strings from '../json/strings.json';
 class HelloWorld extends React.Component {
     render() {
         return <div id="HelloWorld">
+<<<<<<< bfd2f88c7e0ff0984d0ecb09001eca1de46c9bb4
             <Feedback />
             <Link to="/about">{Strings.HelloWorld.link}</Link>
             {Strings.HelloWorld.body}
             <ReportAbuse />
+=======
+            {Strings.HelloWorld.body}
+>>>>>>> add profile
             <br />
             <Link to="/profile">{Strings.HelloWorld.link2}</Link>
             <br />
             <Link to="/about">{Strings.HelloWorld.link1}</Link>
+<<<<<<< bfd2f88c7e0ff0984d0ecb09001eca1de46c9bb4
             <br />
             <Link to="/feedback">{Strings.Feedback.link}</Link>
+=======
+>>>>>>> add profile
         </div>
     }
 }
