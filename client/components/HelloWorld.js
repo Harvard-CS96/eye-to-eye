731e8cd834d45b0d49551d72aefa0882d95b3bd3
Sam Plank <samplank@Sams-MacBook-Pro-2.local>
2017-11-13 13:57:23 -0500

    add profile

--- client/components/HelloWorld.js
+++ client/components/HelloWorld.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Counter from '../containers/Counter';
 import Feedback from '../containers/Feedback';
+import Profile from '../containers/Profile'
+
 import Strings from '../json/strings.json';
 
 class HelloWorld extends React.Component {
     render() {
         return <div id="HelloWorld">
-            {/* {Strings.HelloWorld.body} */}
-            {/* <Counter /> */}
             <Feedback />
             <Link to="/about">{Strings.HelloWorld.link}</Link>
+            {Strings.HelloWorld.body}
+            <br />
+            <Link to="/profile">{Strings.HelloWorld.link2}</Link>
+            <br />
+            <Link to="/about">{Strings.HelloWorld.link1}</Link>
         </div>
     }
 }
