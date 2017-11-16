b6f6d657ba682f479a26122b23605cb3601d4b20
Patrick Pan <patrick.pan@patrickpan.com>
2017-11-15 19:23:59 -0500

    Added question response view under profile

--- client/modules/questions/types.js
+++ client/modules/questions/types.js
@@ -3,5 +3,9 @@ const SET_ANSWER = 'questions/set_answer'
 
 export {
     LOAD_QUESTIONS,
+<<<<<<< 55d71dd27bdb6e4cb017bb20220fbca998937d5c
     SET_ANSWER,
+=======
+    SET_ANSWER
+>>>>>>> Added question response view under profile
 }
