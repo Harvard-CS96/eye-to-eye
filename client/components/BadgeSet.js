bb25711ff43e9c42a0a31734b3907f7f481c2b10
LisaLudique <lisacarolynlu@gmail.com>
2017-11-17 23:48:02 -0500

    Eliminated unique key warning from CriticismSet + BadgeSet

--- client/components/BadgeSet.js
+++ client/components/BadgeSet.js
@@ -11,11 +11,7 @@ class BadgeSet extends React.Component {
         }
         return <div id="BadgeSet">
             <h3>BadgeSet</h3>
-<<<<<<< 0341f52a0760043172870e9cfc4eb3ebc89519bb
             { rows.map((x, i) => ({...x, key: i})) }
-=======
-            {rows}
->>>>>>> Removed counter and HelloWorld default messages.
         </div>
     }
 }
