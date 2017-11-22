ad30464ff8ddc59a9dcb450fa81f20dffa5042ce
LisaLudique <lisacarolynlu@gmail.com>
2017-11-22 15:27:18 -0500

    Merge toggling criticisms and toggling and loading badges

--- client/modules/feedback/operations.js
+++ client/modules/feedback/operations.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import * as actions from './actions';
 
-//TODO: get user id
 const submitFeedback = (rating) => dispatch => {
     console.log("Hit operation");
     console.log(rating);
