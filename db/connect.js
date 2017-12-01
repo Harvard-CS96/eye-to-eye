a98f10f9160d8280e6ce6b6248dcff97ba98d689
AlexAbes <alexcabrahams@gmail.com>
2017-12-01 18:27:58 -0500

    added the fix for the mongodb error, pushing to signaling.tk

--- db/connect.js
+++ db/connect.js
@@ -15,7 +15,12 @@ const db_uri = process.env.DB_URI; // load URI from environment variables
 
 // connect to the mlab instance
 var conn = mongoose.connection;
-conn.openUri(db_uri)
+
+ var options = {
+    keepAlive: 300000,
+    connectTimeoutMS: 30000,
+ };
+conn.openUri(db_uri, options)
     .then(() => {
         console.log("Database: connected to " + db_uri);
     })
