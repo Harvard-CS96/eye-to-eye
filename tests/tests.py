89db103ba4dcfc998f6f64b35e790d8c6b06e8de
Sam Plank <samuelplank@college.harvard.edu>
2017-12-11 17:02:36 -0500

    add feedback test

--- tests/tests.py
+++ tests/tests.py
@@ -5,6 +5,12 @@ from requests import Session
 sess = Session()
 
 def test_landing():
-    """ Tests the landing page response """
-    landing = sess.get("https://eyetoeye.video/")
-    assert landing.status_code == 200 and landing.text
+	""" Tests the landing page response """
+	landing = sess.get("https://eyetoeye.video/")
+	assert landing.status_code == 200 and landing.text
+
+def test_no_feedback():
+	""" Tests that a user can't get to feedback"""
+	feedback = sess.get("https://eyetoeye.video/feedback")
+	assert feedback.status_code == 200 and feedback.text
+
