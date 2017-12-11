61c5174863a8111a16ca59bfc27acbc1cde115d0
Nicholas Boucher <nickboucher32@gmail.com>
2017-12-11 17:02:40 -0500

    Added login test

--- tests/tests.py
+++ tests/tests.py
@@ -8,3 +8,8 @@ def test_landing():
     """ Tests the landing page response """
     landing = sess.get("https://eyetoeye.video/")
     assert landing.status_code == 200 and landing.text
+
+def test_no_anonymous_chat():
+    """ Tests that users must be logged in to chat """
+    client = sess.get("https://eyetoeye.video/chat")
+    assert "facebook.com" in client.url
