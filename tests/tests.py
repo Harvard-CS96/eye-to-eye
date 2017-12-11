ace33d170e8c5b5e5d95a3b7045eff008ccd9e7e
Jacob Lurye <jlurye@college.harvard.edu>
2017-12-11 17:13:54 -0500

    Test that facebook login required to view profile.

--- tests/tests.py
+++ tests/tests.py
@@ -8,3 +8,9 @@ def test_landing():
     """ Tests the landing page response """
     landing = sess.get("https://eyetoeye.video/")
     assert landing.status_code == 200 and landing.text
+
+def test_no_profile_before_login():
+    """ Tests that trying to go to profile before login redirects to facebook """
+    profile = sess.get("https://eyetoeye.video/profile")
+    last_request = profile.history[-1]
+    assert "www.facebook.com" in last_request.url
